cb12c7b3d7c2043c60b3fb83c05c1b3d9d79253f
Gabriel Cazarim <gabrielcazarimdev@gmail.com>
2023-08-08 23:57:02 -0300

    header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
 import { FiTrash2 } from "react-icons/fi";
@@ -7,7 +7,15 @@ import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  return <div>Sidebar</div>;
+  return (
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      }w-full bg-white fixed top-0 h-full shadow-2x1 md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
